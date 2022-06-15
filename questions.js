a100ffe520cea230cd245c9b0f68ce16237f5651
Joey Binas <joseph.t.binas@gmail.com>
2022-06-15 04:07:59 -0700

    "started prompt"

---
+++ questions.js
@@ -0,0 +1,28 @@
+const inquirer = require("inquirer");
+
+function mainMenu() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View a department's budget",
+                "View all roles",
+                "View all employees",
+                "View all employees by department",
+                "View all employees by Manager",
+                "Add a department",
+                "Remove a department",
+                "Add a role",
+                "Remove a role",
+                "Add an employee",
+                "Remove an employee",
+                "Update an employee's role",
+                "Update an employee's manager",
+                "Exit"
+            ]
+        }
+    ])
+}
