350a001c37448ef84d48e9998aea715ba2fc4a8b
Joey Binas <joseph.t.binas@gmail.com>
2022-06-16 21:04:26 -0700

    fixed schema and forkeys

--- db/schema.sql
+++ db/schema.sql
@@ -4,29 +4,25 @@ CREATE DATABASE employeesDB;
 
 USE employeesDB;
 
-CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
-
-);
-
-CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL(9,1) NOT NULL,
-  department_id INT NOT NULL,
-
-  FOREIGN KEY (department_id)
-  REFERENCES department(id)
-);
-
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
-  manager_id INT NULL,
+  manager_id INT NULL
 
-  FOREIGN KEY (role_id)
-  REFERENCES role(id)
 );
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+CREATE TABLE role (
+  id INT AUTO_INCREMENT NOT NULL  PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(9,1) NOT NULL,
+  department_id INT NOT NULL
+
+  
+);
+
